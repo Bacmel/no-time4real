b800e8dccfda01bb19a145808a7d9e50cf50d4c6
Bacmel <hugo.t91800@gmail.com>
2021-02-03 22:14:35 +0100

    Multi Thread

--- project/main.cpp
+++ project/main.cpp
@@ -2,26 +2,77 @@
 #include "pantilt.hpp"
 #include <iostream>
 #include <unistd.h>
+#include <pthread.h>
+
 using namespace std;
+
+#define LIMIT 10.0
+
+typedef struct
+{
+	Donnee d;
+	
+	pthread_t thread_lidar;
+	pthread_t thread_pantilt;
+	
+	pthread_mutex_t mutex_d;
+}Projet;
+
+static Projet projet = 
+{
+	.mutex_d = PTHREAD_MUTEX_INITIALIZER,
+}
+
+static void * fn_lidar(void* p_data);
+static void * fn_pantilt(void* p_data);
+
+static pthread_mutex_t mutex
 
 int main()
 {
-	//Lidar lidar;
-	PanTilt pantilt;
-	/*lidar.start();
-	while(1)
+	ret = pthread_create(& projet.thread_lidar, NULL, fn_lidar, NULL);
+	if(ret)
 	{
-		sleep(1);
-		lidar.update();
-		Donnee d = lidar.getDonnee();
-		cout << "At : " << d.step << ", Data : " << d.distance <<endl;
-		if(d.distance < 10)
-		{
-			break;
-		}
+		fprintf(stderr, "%s", strerror(ret));
 	}
-	lidar.stop();
-	int step = lidar.getDonnee().step+50;
-	*/pantilt.orientation(50);
+	ret = pthread_create(& projet.thread_pantilt, NULL, fn_pantilt, NULL);
+	if(ret)
+	{
+		fprintf(stderr, "%s", strerror(ret));
+	}
+
+	pthread_join(projet.thread_lidar, NULL);
+	pthread_join(projet.thread_pantilt, NULL);
+
 	return 0;
+}
+
+static void * fn_lidar(void* p_data)
+{
+	Lidar lidar;
+	lidar.start();
+	do
+	{
+		lidar.update();
+		pthread_mutex_lock(& projet.mutex_d);
+		projet.d = lidar.getDonnee();
+		pthread_mutex_unlock(& projet.mutex_d);
+	}while(true);
+	lidar.stop();
+}
+
+static void * fn_pantilt(void* p_data)
+{
+	PanTilt pantilt;
+	Donnee d;
+	do
+	{
+		pthread_mutex_lock(& projet.mutex_d);
+		d = projet.d;
+		pthread_mutex_unlock(& projet.mutex_d);
+		if(d.distance < LIMIT)
+		{
+			pantilt.orientation(d.step);
+		}
+	}while(true);
 }
